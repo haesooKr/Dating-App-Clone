d3076bb7b91d4babafe2e4413e0a0bca208c12c2
haesooKr <gotn676@gmail.com>
2020-05-29 13:23:42 -0400

    Connect MongoDB

---
+++ models/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/Tinder", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, (err) => {
+  if(!err){
+    console.log('MongoDB is connected successfully');
+  } else {
+    console.error(err);
+  }
+});
+
+module.exports = mongoose;
