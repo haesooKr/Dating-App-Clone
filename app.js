e49f06eddf3036eab246087ded1139b5e3f8008e
haesooKr <gotn676@gmail.com>
2020-05-29 13:19:54 -0400

    Create express server

---
+++ app.js
@@ -0,0 +1,21 @@
+// dev
+
+require('dotenv').config();
+const logger = require('morgan');
+
+// /dev
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
