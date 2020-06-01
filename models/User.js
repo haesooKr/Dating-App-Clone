d77de4c6e40548298cb76af4c135004ed4feb8e7
haesooKr <gotn676@gmail.com>
2020-06-01 15:27:25 -0400

    Add first & last name

--- models/User.js
+++ models/User.js
@@ -13,6 +13,14 @@ const UserSchema = mongoose.Schema({
     type : String,
     required: true
   },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     required: true,
