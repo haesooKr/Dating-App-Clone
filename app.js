1c2806dc0d93f21184b0005986699b9de67ef2d1
haesooKr <gotn676@gmail.com>
2020-06-02 17:38:02 -0400

    Connect imageRouter to app.js

--- app.js
+++ app.js
@@ -10,15 +10,20 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const userRouter = require('./routes/User');
+const imageRouter = require('./routes/Image');
+
+const methodOverride = require('method-override');
 
 const app = express();
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/user', userRouter)
+app.use('/user', userRouter);
+app.use('/image', imageRouter);
 
 
 app.listen(PORT, () => {
